d2d26b4402b609dde02a84382d4652527dd7fa36
ddgf <godoyfont@gmail.com>
2023-03-22 10:34:52 -0300

    fix bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ddgf.github.io/',
+  base: 'https://ddgf.github.io/gh-react-vite/',
   plugins: [react()],
 })
